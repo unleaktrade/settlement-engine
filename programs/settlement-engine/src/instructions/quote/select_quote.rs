4381582f0eb46c3ae65214b0f89399da1d3c701c
_whyvrafvr <jsie@trendev.fr>
2025-11-24 20:42:03 +0100

    constraints for init settlement

--- programs/settlement-engine/src/instructions/quote/select_quote.rs
+++ programs/settlement-engine/src/instructions/quote/select_quote.rs
@@ -1,5 +1,6 @@
 use crate::state::rfq::{Rfq, RfqState};
 use crate::state::Quote;
+use crate::state::Settlement;
 use crate::{state::config::Config, RfqError, QuoteError};
 use anchor_lang::prelude::*;
 
@@ -20,18 +21,24 @@ pub struct SelectQuote<'info> {
     #[account(
         mut,
         has_one = rfq,
-        constraint = !quote.is_revealed() @ QuoteError::QuoteAlreadyRevealed,
-    )]
+        constraint = quote.is_revealed() @ RfqError::InvalidState,)]
     pub quote: Account<'info, Quote>,
-    //@TODO: create settlement account
 
-     pub system_program: Program<'info, System>,
+    #[account(
+        init,
+        payer = maker,
+        space = 8 + Settlement::INIT_SPACE,
+        seeds = [Settlement::SEED_PREFIX, rfq.key().as_ref()],
+        bump,
+    )]
+    pub settlement: Account<'info, Settlement>,
+
+    pub system_program: Program<'info, System>,
 }
 
 pub fn select_quote_handler(ctx: Context<SelectQuote>, quote_key: Pubkey) -> Result<()> {
     let now = Clock::get()?.unix_timestamp;
     let rfq = &mut ctx.accounts.rfq;
-    require!(rfq.state == RfqState::Revealed, RfqError::InvalidState);
     let selection_deadline = rfq.selection_deadline().ok_or(RfqError::InvalidState)?;
     require!(now <= selection_deadline, RfqError::TooLate);
     require!(rfq.selected_quote.is_none(), RfqError::AlreadySelected);
