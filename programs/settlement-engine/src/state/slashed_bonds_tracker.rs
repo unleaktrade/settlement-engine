f5d889384dd7d6199a62c8e82407840ccfb41611
_whyvrafvr <jsie@trendev.fr>
2025-12-08 22:59:39 +0100

    Add helper

--- programs/settlement-engine/src/state/slashed_bonds_tracker.rs
+++ programs/settlement-engine/src/state/slashed_bonds_tracker.rs
@@ -13,4 +13,8 @@ pub struct SlashedBondsTracker {
 
 impl SlashedBondsTracker {
     pub const SEED_PREFIX: &'static [u8] = b"slashed_bonds_tracker";
+    
+    pub fn is_seized(&self) -> bool {
+        self.amount.is_some() && self.seized_at.is_some()
+    }
 }
